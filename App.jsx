ad924c23312d9adbda054ddc04f7162d9d22d064
Pranavaditya Sheshendra <pranavaditya2603@gmail.com>
2026-02-18 16:31:08 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
