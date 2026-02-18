6e24db825e6a4798b594330c15b6cc38265c0b0c
Pranavaditya Sheshendra <pranavaditya2603@gmail.com>
2026-02-18 16:29:49 +0530

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide high quality indoor and outdoor plants.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
